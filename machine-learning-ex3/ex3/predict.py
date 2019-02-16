5e078571251713cc641d9b8f787ec65d8ae87c76
woodhfut <woodhfut@hotmail.com>
2019-02-16 16:35:22 +0800

    ex3_nn finished
    ex3_nn

--- machine-learning-ex3/ex3/predict.py
+++ machine-learning-ex3/ex3/predict.py
@@ -5,7 +5,7 @@ def predict(theta1, theta2, x):
     # Useful values
     m = x.shape[0]
     num_labels = theta2.shape[0]
-
+    print('shape x {}, theta1 {}, theta2 {}'.format(x.shape, theta1.shape, theta2.shape))
     # You need to return the following variable correctly
     p = np.zeros(m)
 
@@ -14,7 +14,15 @@ def predict(theta1, theta2, x):
     #                your learned neural network. You should set p to a
     #                1-D array containing labels between 1 to num_labels.
     #
+    x = np.c_[np.ones((m,1)), x]
 
+    a2 = sigmoid(np.dot(x, theta1.T))
+
+    a2 = np.c_[np.ones((m,1)), a2]
+
+    a3 = sigmoid(np.dot(a2, theta2.T))
+
+    p = np.argmax(a3, axis=1)+1
 
     return p
 
